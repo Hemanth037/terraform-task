f7a30df07b67f3e8af20cca7cd65aa63cb01be70
hemanthkatta07@gmail.com <hemanthkatta07@gmail.com>
2025-07-17 21:22:47 +0000

    second

--- resource.tf
+++ resource.tf
@@ -1,25 +1,19 @@
-resource "azurerm_resource_group" "terra" {
-  name     = "terra"
+resource "azurerm_resource_group" "practice-terra05" {
+  name     = "terra-practice"
   location = "East US"
 }
 
-resource "azurerm_virtual_network" "terra_vnet" {
-    name                = "terra-vnet"
+resource "azurerm_virtual_network" "practice-vnet05" {
+    name                = "vnet-practice"
     address_space       = ["10.0.0.0/16"]
-    location            = azurerm_resource_group.terra.location
-    resource_group_name = azurerm_resource_group.terra.name
+    location            = azurerm_resource_group.practice-terra05.location
+    resource_group_name = azurerm_resource_group.practice-terra05.name
 }
  
 resource "azurerm_subnet" "subnet1" {
     name                 = "subnet1"
-    resource_group_name  = azurerm_resource_group.terra.name
-    virtual_network_name = azurerm_virtual_network.terra_vnet.name
+    resource_group_name  = azurerm_resource_group.practice-terra05.name
+    virtual_network_name = azurerm_virtual_network.practice-vnet05.name
     address_prefixes     = ["10.0.1.0/24"]
 }
  
-resource "azurerm_subnet" "subnet2" {
-    name                 = "subnet2"
-    resource_group_name  = azurerm_resource_group.terra.name
-    virtual_network_name = azurerm_virtual_network.terra_vnet.name
-    address_prefixes     = ["10.0.2.0/24"]
-}
